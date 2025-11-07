7804378eb8c5f0b81399a029224730365bb760cf
CrstKng <rmn.l.dimitrov@gmail.com>
2025-11-07 22:30:35 +0100

    added calculator for testing; prompts are in a list; added --verbose

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-
-
+from google.genai import types
+import sys
 
 def main():
-    #print("Hello from ai-agent!")
-    pass
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    try: 
+        user_prompt = sys.argv[1]
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        response = client.models.generate_content(
+            model = 'gemini-2.0-flash-001',
+            contents = messages,
+        )
+    except IndexError:
+        print("you need to provide a prompt")
+        sys.exit(1)
+
+    for arg in sys.argv:
+        if arg == "--verbose":
+            print(f"User prompt: {response.text}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
